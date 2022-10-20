50cd8ff7b69e4aa9b5548fef2fd9ab0334b101f1
Max <88722440+Libes6@users.noreply.github.com>
2022-10-20 20:59:42 +0700

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./scss/index.scss"
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Content from './components/Content/Content';
+import Cart from './components/Cart/index'
+
 function App() {
 
   return (
    <div class="main">
-   <Header/>
-   <Menu/>
-   <Content/>
+      <Header/>
+       <Menu/>
+       <Content/>
   </div>
   );
 }
--- src/components/Content/Cart.jsx
+++ src/components/Content/Cart.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import style from "./Cart.module.scss";
 const Cart = () => {
   return (
-    <div className={style.Cart}>
-      <div className={style.img}>
-        <img src="http://placeimg.com/640/480/transport" alt="" />
-      </div>
-      <h4>Samurai</h4>
+     <div className={style.Cart}>
+         <div className={style.img}>
+            <img src="http://placeimg.com/640/480/transport" alt="" />
+        </div>
+    <h4>Samurai</h4>
       <div className={style.CartButoom}>
         <div className={style.Price}>
            от 540 $
@@ -17,4 +17,8 @@ const Cart = () => {
   );
 };
 
+
+
+
 export default Cart;
+
